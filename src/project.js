0f490b18de812dbd1df86ed0644943ac31510b99
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2025-01-14 20:03:15 +0700

    Add ManageProject function factory to project modules

--- src/project.js
+++ src/project.js
@@ -13,3 +13,53 @@ class Todo {
     this.priority = priority;
   }
 }
+
+export default function ManageProject(){
+  const projects = [];
+
+  const newProject = (name) => {
+    projects.push(new Project(name));
+    console.log(projects);
+  };
+
+  const addTodoList = ( projectIndex,
+    title,
+    dueDate,
+    description,
+    priority
+    ) => {
+      projects[projectIndex].todoList.push(new Todo(
+        title, 
+        dueDate, 
+        description, 
+        priority
+      ));
+  };
+
+  const getProjectList = () => {
+    const projectList = [];
+    projects.forEach((project) => {
+      projectList.push(project);
+    });
+    return projectList;
+  };
+
+  const getProjectTodoList = (projectIndex) => {
+    const todoList = [];
+    todoList.push(projects[projectIndex].todoList);
+    console.log(todoList);
+    return todoList;
+  };
+
+  const deleteProject = (projectIndex) => {
+    projects.splice(projectIndex, 1);
+  }
+
+  return {
+    newProject,
+    addTodoList,
+    getProjectList,
+    getProjectTodoList,
+    deleteProject
+  }
+}
