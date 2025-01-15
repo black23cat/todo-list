f0531e150ab1d83e65dcac9d939a634fe6f4a807
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2025-01-15 07:30:37 +0700

    add event listener to content container and running function to generate content items add function to hide/show new todo form setting attribute for min date for new todo form add generateContentDisplay fn to render todo card add new todo form to html template

--- src/event.js
+++ src/event.js
@@ -1,20 +1,20 @@
 import ManageProject from './project.js';
 import ScreenDisplay from './screen-display.js';
 
-
-export default function event(){
+function event(){
   const project = ManageProject();
   const screen = ScreenDisplay();
   const sidebar = document.querySelector('.sidebar');
   const newProjectForm = document.querySelector('.new-project-form');
-  const projectsList = document.querySelector('.projects-list');
+  const addTodoForm = document.querySelector('.add-todo');
+  const dueDateInput = document.querySelector('[name=due-date]');
   const content = document.querySelector('.content');
   const mainContent = document.querySelector('.main-content');
-
+  const projectsList = document.querySelector('.projects-list');
   let currentProjectIndex;
-
-
+  dueDateInput.setAttribute('min', project.getTodayDate());
   sidebar.addEventListener('click', sidebarEventHandler);
+  content.addEventListener('click', contentEventHandler);
 
   function sidebarEventHandler(event){
     const target = event.target;
@@ -41,23 +41,43 @@ export default function event(){
     }
     if(target.classList.contains('project-btn')){
       currentProjectIndex = target.dataset.index;
-      screen.generateContentDisplay(mainContent);
+      screen.generateContentDisplay(
+        mainContent, 
+        project.getProjectTodoList(currentProjectIndex)
+      );
       console.log(currentProjectIndex);
       return;
     }
   }
 
+  function contentEventHandler(event){
+    const target = event.target;
 
+    if(target.classList.contains('new-todo')){
+      addTodoForm.style.display = 'block';
+      return;
+    }
 
+    if(target.id === 'submit'){
+      const formInput = [...addTodoForm.getElementsByTagName('input')].map(input => input.value);
+      formInput.push(document.getElementById('priority').value);
 
-
-
-
-
-
-
-
-
-
-
+      if(formInput.includes('')){
+        alert('Fill the fields');
+        return;
+      }
+      project.newTodoList(currentProjectIndex, formInput);
+      screen.generateContentDisplay(
+        mainContent, 
+        project.getProjectTodoList(currentProjectIndex)
+      );
+      addTodoForm.style.display = 'none';
+    }
+    if(target.id === 'cancel'){
+      addTodoForm.reset();
+      addTodoForm.style.display = 'none';
+    }
+  }
 }
+
+export { event }
--- src/screen-display.js
+++ src/screen-display.js
@@ -1,37 +1,73 @@
 export default function ScreenDisplay(){
-  const generateContentDisplay = ( parentNode ) => {
+  const updateContentDisplay = ( parentNode, array) => {
     clearParentNode( parentNode );
     const addTodoBtn = document.createElement('button');
     addTodoBtn.classList.add('new-todo');
     addTodoBtn.textContent = '+';
     parentNode.appendChild( addTodoBtn );
+    generateContentDisplay(parentNode, array);
   }
   
   const updateProjectDisplay = ( parentNode, array ) => {
     clearParentNode( parentNode );
-    array.forEach(( obj, index ) => {
+    for(let i = 0; i < array.length ; i ++){
       const projectBtnWrapper = document.createElement('div');
       const projectName = document.createElement('button');
       const deleteProjectBtn = document.createElement('button');
       projectBtnWrapper.classList.add('project-list-wrapper');
-      projectName.textContent = obj.projectName;
+      projectName.textContent = array[i].projectName;
       projectName.classList.add('project-btn');
-      projectName.setAttribute('data-index', index);
+      projectName.setAttribute('data-index', i);
       deleteProjectBtn.textContent = 'X';
       deleteProjectBtn.classList.add('delete-project');
       projectBtnWrapper.appendChild(projectName);
       projectBtnWrapper.appendChild(deleteProjectBtn);
       parentNode.appendChild(projectBtnWrapper);
-    });
+    };
   }
   
+  const generateContentDisplay = ( parentNode, array ) => {
+    if(array.length === 0){
+      const contentPara = document.createElement('p');
+      contentPara.textContent = 'No tasks.';
+      parentNode.appendChild(contentPara);
+    }
+    for(let i = 0 ; i < array.length ; i++ ){
+      const card = document.createElement('div');
+      const title = document.createElement('h4');
+      const dueDate = document.createElement('p');
+      const description = document.createElement('p');
+      const priority = document.createElement('span');
+      const deleteTodoBtn = document.createElement('button');
+  
+      card.classList.add('todo-card');
+      card.setAttribute('data-todo-index', i);
+      title.textContent = array[i].title;
+      dueDate.textContent = array[i].dueDate;
+      description.textContent = array[i].description;
+      priority.textContent = array[i].priority;
+      priority.style.backgroundColor = 
+        (array[i].priority === 'low') ? 'lightgreen' :
+        (array[i].priority === 'medium') ? '#FFB52E' :
+        '#FF0000';
+      deleteTodoBtn.textContent = 'x';
+
+      card.appendChild(title);
+      card.appendChild(description);
+      card.appendChild(dueDate);
+      card.appendChild(priority);
+      card.appendChild(deleteTodoBtn);      
+      parentNode.appendChild(card);
+    }
+    console.log(parentNode)
+  }
 
   const clearParentNode = (parentNode) => {
     parentNode.textContent = '';
   }
 
   return {
-    generateContentDisplay,
+    updateContentDisplay,
     updateProjectDisplay
   }
 }
